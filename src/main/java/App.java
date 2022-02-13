36e08e011ad831190ecff72c213254803b843b80
Bruno Carvalho Xavier <bruno.cxavier@hotmail.com>
2022-02-13 12:44:43 -0300

    Removendo dinamico e adicionando o jupyter notebook

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,4 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
-import model.DynamicResponse;
 import model.GreedyResponse;
 import model.Instance;
 
@@ -15,11 +14,8 @@ public class App {
         ObjectMapper objectMapper = new ObjectMapper();
         File file = new File(args[0]);
         Instance instance = objectMapper.readValue(file, Instance.class);
-        //Dynamic dynamic = new Dynamic();
-        //DynamicResponse dynamicResponse = dynamic.calculate(instance);
         Greedy greedy = new Greedy();
         GreedyResponse greedyResponse = greedy.calculate(instance);
-        //System.out.println(dynamicResponse);
         System.out.println(greedyResponse);
     }
 }
